a7d7a40cd7d677eebcc1d52da5e6b8fd581cd380
Andrzej Trzaska <atrzaska2@gmail.com>
2021-06-09 23:16:47 +0200

    Add support for list filtering

--- src/helpers/filter.js
+++ src/helpers/filter.js
@@ -1,22 +1,39 @@
+const SEPARATOR = ','
+
+const parse = (value) => {
+  let parsed = null
+
+  if (value.includes(SEPARATOR)) {
+    return value.split(SEPARATOR).map((v) => parse(v))
+  }
+
+  if (value === 'true') {
+    parsed = true
+  } else if (value === 'false') {
+    parsed = false
+  } else if (value === 'null') {
+    parsed = null
+  } else if (!isNaN(value)) {
+    parsed = parseInt(value)
+  } else {
+    parsed = value
+  }
+
+  return parsed
+}
 const filter = (results, req) => {
   for (const [key, value] of Object.entries(req.query)) {
     if (key.startsWith('filter.')) {
       const field = key.replace('filter.', '')
-      let parsed = null
+      const parsedValue = parse(value)
 
-      if(value === 'true') {
-        parsed = true
-      } else if (value === 'false') {
-        parsed = false
-      } else if (value === 'null') {
-        parsed = null
-      } else if (!isNaN(value)) {
-        parsed = parseInt(value)
-      } else {
-        parsed = value
-      }
-
-      results = results.filter(x => x[field] === parsed)
+      results = results.filter((r) => {
+        if (Array.isArray(parsedValue)) {
+          return parsedValue.includes(r[field])
+        } else {
+          return r[field] === parsedValue
+        }
+      })
     }
   }
 
