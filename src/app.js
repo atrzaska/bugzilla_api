ac9618b15fe3ae2c3602cf9540392c14ada5c897
Andrzej Trzaska <atrzaska2@gmail.com>
2021-07-08 15:48:07 +0200

    Add cors and cookie parser

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const morgan = require('morgan')
+const cookieParser = require('cookie-parser')
+const cors = require('cors')
 const { NotFoundError } = require('src/services/errors')
 const apiRoutes = require('src/routes/api')
 
@@ -8,6 +10,8 @@ const port = 4000
 
 app.use(morgan('dev'))
 app.use(express.json())
+app.use(cookieParser())
+app.use(cors({ origin: 'http://localhost:8080', credentials: true }))
 app.use('/api', apiRoutes)
 app.use((err, req, res, next) => {
   console.log(err)
